f956d0949404e277f0428e9e717fc7458d7d3f52
hiro-asano <hiro_asano@r.recruit.co.jp>
2022-09-08 23:45:16 +0900

    fix top

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,42 +1,43 @@
 <script lang="ts">
   import { Container, Row, Col } from 'sveltestrap'
-  import { ContestsApi, Configuration, PublicEntry, Match } from './generated'
+  import { ContestsApi, Configuration, Contest } from './generated'
   import LeaderBoard from './components/LeaderBoard.svelte'
-  import Status from "./components/Status.svelte";
+  import Status from './components/Status.svelte'
   import Pickup from './components/Pickup.svelte'
-  import History from "./components/History.svelte";
+  import History from './components/History.svelte'
 
-  let entries: PublicEntry[] = []
-  let matches: Match[] = []
-
-  async function fetchData() {
+  async function fetchData(): Promise<Contest> {
     const client = new ContestsApi(new Configuration({
       basePath: 'http://localhost:8081',
     }))
     const contests = await client.getContests()
-    const contest = await client.getContestById({ contestId: contests[0].id })
-    entries = contest.ranking
-    matches = contest.recentMatches
+    return client.getContestById({ contestId: contests[0].id })
   }
 
   const promise = fetchData()
 </script>
 
-<Container>
-  <Row>
-    <h1>CodeBattle</h1>
-  </Row>
-  <Row>
-    <Col xs="8">
-      <Pickup/>
-      <LeaderBoard entries={entries}/>
-    </Col>
-    <Col xs="4">
-      <Status />
-      <History matches={matches} />
-    </Col>
-  </Row>
-</Container>
+{#await promise}
+  <div class="d-flex justify-content-center align-items-center" style="height: 100%">
+    <div class="spinner-border" role="status"></div>
+  </div>
+{:then contest}
+  <Container>
+    <Row>
+      <h1>{contest.name}</h1>
+    </Row>
+    <Row>
+      <Col xs="8">
+        <Pickup/>
+        <LeaderBoard entries={contest.ranking}/>
+      </Col>
+      <Col xs="4">
+        <Status/>
+        <History matches={contest.recentMatches}/>
+      </Col>
+    </Row>
+  </Container>
+{/await}
 
 <style>
 </style>
