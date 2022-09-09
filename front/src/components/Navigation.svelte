29c008a37132bff3b7624928d657d00a204eaf5d
hiro-asano <hiro_asano@r.recruit.co.jp>
2022-09-10 00:10:13 +0900

    add navigation

---
+++ front/src/components/Navigation.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import {
+    Collapse,
+    Dropdown,
+    DropdownItem,
+    DropdownMenu, DropdownToggle,
+    Nav,
+    Navbar,
+    NavbarBrand,
+  } from 'sveltestrap'
+</script>
+
+<Navbar color="light" light expand="md">
+  <NavbarBrand href="#">Code Battle</NavbarBrand>
+  <Collapse navbar isOpen>
+    <Nav class="ms-auto" navbar>
+      <Dropdown nav inNavbar>
+        <DropdownToggle nav caret>Menu</DropdownToggle>
+        <DropdownMenu end>
+          <DropdownItem>Status</DropdownItem>
+          <DropdownItem divider/>
+          <DropdownItem>Logout</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </Nav>
+  </Collapse>
+</Navbar>
+
+<style>
+</style>
