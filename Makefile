3ea962b86f4cbdce026beee31d8c030c3608eb4e
hiro-asano <hiro_asano@r.recruit.co.jp>
2022-09-11 18:49:12 +0900

    add server code

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-generate:
+generate-client:
 	docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
 		-i /local/server/openapi.yaml -g typescript-fetch -o /local/front/generated/
+generate-server:
+	docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
+		-i /local/server/openapi.yaml -g go-gin-server -o /local/server/api/
 mock:
 	prism mock -p 8081 --cors server/openapi.yaml
--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -1,11 +1,16 @@
+DROP TABLE IF EXISTS user;
 CREATE TABLE user
 (
-    id       VARCHAR(36)  NOT NULL,
-    name     VARCHAR(100) NOT NULL,
-    is_admin BOOL         NOT NULL DEFAULT FALSE,
+    id         VARCHAR(36)  NOT NULL,
+    name       VARCHAR(100) NOT NULL,
+    icon       VARCHAR(100) NOT NULL,
+    is_admin   BOOL         NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP    NOT NULL,
     PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS contest;
 CREATE TABLE contest
 (
     id          VARCHAR(36)   NOT NULL,
@@ -13,10 +18,13 @@ CREATE TABLE contest
     name        VARCHAR(100)  NOT NULL,
     description VARCHAR(1000),
     repository  VARCHAR(1000) NOT NULL,
+    created_at  TIMESTAMP     NOT NULL,
+    updated_at  TIMESTAMP     NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (owner) REFERENCES user (id)
 );
 
+DROP TABLE IF EXISTS entry;
 CREATE TABLE entry
 (
     id         VARCHAR(36)   NOT NULL,
@@ -24,22 +32,28 @@ CREATE TABLE entry
     contest_id VARCHAR(36)   NOT NULL,
     name       VARCHAR(100)  NOT NULL,
     repository VARCHAR(1000) NOT NULL,
-    status     int           NOT NULL,
+    status     INT           NOT NULL,
     error      VARCHAR(1000),
+    score      INT           NOT NULL,
+    created_at TIMESTAMP     NOT NULL,
+    updated_at TIMESTAMP     NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES user (id),
     FOREIGN KEY (contest_id) REFERENCES contest (id)
 );
 
+DROP TABLE IF EXISTS `match`;
 CREATE TABLE `match`
 (
     id           VARCHAR(36) NOT NULL,
     entry_id     VARCHAR(36) NOT NULL,
     contest_id   VARCHAR(36) NOT NULL,
-    `rank`       int         NOT NULL,
-    before_score int         NOT NULL,
-    after_score  int         NOT NULL,
-    PRIMARY KEY (id),
+    `rank`       INT         NOT NULL,
+    before_score INT         NOT NULL,
+    after_score  INT         NOT NULL,
+    created_at   TIMESTAMP   NOT NULL,
+    updated_at   TIMESTAMP   NOT NULL,
+    PRIMARY KEY (id, entry_id),
     FOREIGN KEY (entry_id) REFERENCES entry (id),
     FOREIGN KEY (contest_id) REFERENCES contest (id)
 );
